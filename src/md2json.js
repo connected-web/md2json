a46e58bf71b7f625bf6ffdbf5d429b15267b6241
John Beech <jbeech@marqeta.com>
2021-10-14 11:41:36 +0100

    Add additional test

--- src/md2json.js
+++ src/md2json.js
@@ -3,7 +3,17 @@ const createTokensFromDom = require('./steps/createTokensFromDom')
 const throwAwayInvalidTokens = require('./steps/throwAwayInvalidTokens')
 const parseTokensIntoHierarchy = require('./steps/parseTokensIntoHierarchy')
 
-function md2json (title, md) {
+function formatParams (params) {
+  const md = params.pop()
+  const title = params.pop() || md.title || ''
+  return {
+    title,
+    md: md.md || md.markdown || md
+  }
+}
+
+function md2json (...params) {
+  const { title, md } = formatParams(params)
   const $dom = $createDomFromMarkdown(md)
   const tokens = createTokensFromDom($dom)
   const tokensOfInterest = throwAwayInvalidTokens(tokens)
--- test/md2json.test.js
+++ test/md2json.test.js
@@ -14,7 +14,27 @@ async function loadJsonFixture (fixture) {
 }
 
 describe('Markdown 2 Json', () => {
-  it('should convert markdown to json', async () => {
+  it('should convert a small section of md to json', async () => {
+    const source = [
+      '# Heading',
+      'Basic content'
+    ].join('\n')
+    const actual = md2json(source)
+    const expected = {
+      title: '',
+      content: [],
+      sections: [{
+        title: 'Heading',
+        content: [
+          'Basic content'
+        ],
+        sections: []
+      }]
+    }
+    expect(actual).to.deep.equal(expected)
+  })
+
+  it('should convert markdown to json with a title', async () => {
     const filename = 'example-file-input.md'
     const source = await loadFixture('example-file-input.md')
     const expected = await loadJsonFixture('expected-file-output.json')
